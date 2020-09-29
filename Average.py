4828141acea991c46405aa6c2ec79bb6c2a7a5a3
Andrew Catapano <71726115+acatapano@users.noreply.github.com>
2020-09-29 14:33:02 -0400

    This is the second version.

--- Average.py
+++ Average.py
@@ -1,5 +1,12 @@
+avergae = 0.0
+numberOfTests = 0
+score = 0
+scoreCount = 0
+total = 0
+
 numberOfTests = int(input("Please enter the number of tests you want to average: "))
-score1 = int(input("Please enter a score: "))
-score2 = int(input("Please enter a score: "))
-average = (score1 + score2)/2
+score = int(input("Please enter a score: "))
+scoreCount = scoreCount + 1
+total = total + score   #Can also be total += score
+average = total/scoreCount
 print("The average is ", average)
