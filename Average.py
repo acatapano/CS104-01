53d32c8d970672f192ea558617c9f85a3c6bb1f3
Andrew Catapano <71726115+acatapano@users.noreply.github.com>
2020-09-30 12:55:23 -0400

    Third Version (Added While Loop)

--- Average.py
+++ Average.py
@@ -1,12 +1,16 @@
-avergae = 0.0
+average = 0.0
 numberOfTests = 0
 score = 0
 scoreCount = 0
 total = 0
 
 numberOfTests = int(input("Please enter the number of tests you want to average: "))
-score = int(input("Please enter a score: "))
-scoreCount = scoreCount + 1
-total = total + score   #Can also be total += score
+
+#Make these 3 lines repeat until scoreCount = numberOfTests
+while scoreCount < numberOfTests:
+    score = int(input("Please enter a score: "))
+    scoreCount = scoreCount + 1
+    total = total + score   #Can also be total += score
+
 average = total/scoreCount
 print("The average is ", average)
