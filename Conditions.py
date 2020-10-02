00d88f6a6e52498b770e067fe4e42cfc6fdb65e1
Andrew Catapano <71726115+acatapano@users.noreply.github.com>
2020-10-02 12:54:43 -0400

    Second Version w/Loop

--- Conditions.py
+++ Conditions.py
@@ -1,14 +1,15 @@
 temp = 0
 
-temp = int(input("Please enter current temperature: "))
+while temp < 999:
+    temp = int(input("Please enter current temperature: "))
 
-if temp > 90:
-    print("Wear Shorts")
-elif 90 >= temp > 70:
-    print("Short Sleeves are Fine")
-elif 70 >= temp > 50:
-    print("Wear a Jacket")
-elif 50 >= temp > 32:
-    print("Wear a Heavy Coat")
-else:
-    print("Stay Inside")
+    if temp > 90:
+        print("Wear Shorts")
+    elif 90 >= temp > 70:
+        print("Short Sleeves are Fine")
+    elif 70 >= temp > 50:
+        print("Wear a Jacket")
+    elif 50 >= temp > 32:
+        print("Wear a Heavy Coat")
+    else:
+        print("Stay Inside")
